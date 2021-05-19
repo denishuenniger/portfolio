e2eb5e56f1ff3e2b2560c8caac3d94921961f025
Denis Huenniger <denis.huenniger@htw-dresden.de>
2021-05-19 21:25:14 +0200

    debug

--- projects/static/forms/contact.php
+++ projects/static/forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => '127.0.0.1',
+    'username' => 'denishuenniger@pm.me',
+    'password' => 'VOfnUCef13lMk-ST7kGOBA',
+    'port' => '1025'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
